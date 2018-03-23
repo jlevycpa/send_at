0ba0cd0e3ef34d3574739d84b9b76cb162ea5e1d
Jeff Levy <jeff@jefflevy.org>
2018-03-23 11:51:19 -0400

    change to application

--- lib/send_at/application.ex
+++ lib/send_at/application.ex
@@ -6,16 +6,22 @@ defmodule SendAt.Application do
   use Application
 
   def start(_type, _args) do
+    do_start(:erlang.system_info(:time_warp_mode))
+  end
+  
 
-    children = case :erlang.system_info(:time_warp_mode) do
-      :multi_time_warp -> [SendAt.TimerServer]
-      :no_time_warp -> []
-      :single_time_warp -> []
-    end
+  defp do_start(:multi_time_warp) do
+    children = [SendAt.TimerServer]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: SendAt.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  defp do_start(_) do
+    {:ok, self()}
+  end
+
+
 end
